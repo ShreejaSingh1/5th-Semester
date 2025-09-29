8a806148558ce67dcf775f82dcd7ca3ad2b746d8
Shreeja Singh <145353903+ShreejaSingh1@users.noreply.github.com>
2025-09-29 22:37:30 +0530

    Time: 3 ms (84.37%) | Memory: 49.4 MB (73.02%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,24 +1,25 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        if (grid == null || grid.length == 0) return 0;
-        int m = grid.length, n = grid[0].length, count = 0;
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        int count = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    mark(grid, i, j); 
                 }
             }
         }
         return count;
     }
-    
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length || grid[i][j] == '0') return;
-        grid[i][j] = '0';
-        dfs(grid, i + 1, j);
-        dfs(grid, i - 1, j);
-        dfs(grid, i, j + 1);
-        dfs(grid, i, j - 1);
+
+    void mark(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == '0')
+            return;
+
+        grid[i][j] = '0'; 
+        mark(grid, i + 1, j);
+        mark(grid, i - 1, j);
+        mark(grid, i, j + 1);
+        mark(grid, i, j - 1);
     }
 }
