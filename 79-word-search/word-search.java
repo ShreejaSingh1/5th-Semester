7518b7c4859c23dad2a7530549038bf61c30167a
Shreeja Singh <145353903+ShreejaSingh1@users.noreply.github.com>
2025-10-04 14:28:42 +0530

    Time: 132 ms (78.83%) | Memory: 41.4 MB (89.48%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,33 +1,40 @@
 class Solution {
-    public boolean exist(char[][] grid, String word) {
-        int rows = grid.length;
-        int cols = grid[0].length;
-
-        for (int r = 0; r < rows; r++) {
-            for (int c = 0; c < cols; c++) {
-                if (search(grid, word, r, c, 0)) {
-                    return true;
-                }
+    public boolean exist(char[][] board, String word) {
+        for (int i=0;i<board.length;i++){
+            for(int j=0;j<board[0].length;j++){
+                
+                    if(helper(board, word, i, j,0)){
+                        return true;
+                    }
+                
             }
         }
         return false;
     }
 
-    private boolean search(char[][] grid, String word, int row, int col, int index) {
-        if (index == word.length()) return true;
-        if (row < 0 || col < 0 || row >= grid.length || col >= grid[0].length) return false;
-        if (grid[row][col] != word.charAt(index)) return false;
+    public boolean helper(char[][] board, String word, int i, int j,int idx){
+        if(idx==word.length()){
+            return true;
+        }
+        if(i<0 || i>=board.length || j<0 || j>=board[0].length || board[i][j] != word.charAt(idx)){
+            return false;
+        }
+        
 
-        char currentChar = grid[row][col];
-        grid[row][col] = '*';
+        char temp =  board[i][j];
+        board[i][j]='*';
+        
+        if(
+            helper(board,word,i+1,j,idx+1) || 
+            helper(board,word,i,j+1,idx+1) ||
+            helper(board,word,i-1,j,idx+1) ||
+            helper(board,word,i,j-1,idx+1)
+        ){
+            return true;
+        }
+        
+        board[i][j] = temp;
 
-        boolean exists = search(grid, word, row + 1, col, index + 1) ||
-                         search(grid, word, row - 1, col, index + 1) ||
-                         search(grid, word, row, col + 1, index + 1) ||
-                         search(grid, word, row, col - 1, index + 1);
-
-        grid[row][col] = currentChar;
-
-        return exists;
+        return false;
     }
 }
