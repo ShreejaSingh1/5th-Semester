07a9f8a44146153e78e95c64c376732ed77ca621
Shreeja Singh <145353903+ShreejaSingh1@users.noreply.github.com>
2025-10-06 15:24:42 +0530

    Time: 18 ms (36.76%) | Memory: 56.9 MB (40.81%) - LeetSync

--- 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
+++ 2538-minimum-cost-to-make-array-equal/minimum-cost-to-make-array-equal.java
@@ -1,31 +1,34 @@
+import java.util.*;
+
 class Solution {
     public long minCost(int[] nums, int[] cost) {
-        int low = Integer.MAX_VALUE, high = Integer.MIN_VALUE;
-        for(int num : nums){
-            low = Math.min(low, num);
-            high = Math.max(high, num);
+        int n = nums.length;
+        int[][] arr = new int[n][2];
+        for(int i = 0; i < n; i++){
+            arr[i][0] = nums[i];
+            arr[i][1] = cost[i];
         }
 
-        while(low < high){
-            int mid = low + (high - low)/2;
-            long costMid = totalCost(nums, cost, mid);
-            long costMidPlus = totalCost(nums, cost, mid+1);
+        Arrays.sort(arr, (a,b) -> Integer.compare(a[0], b[0]));
 
-            if(costMid > costMidPlus){
-                low = mid + 1;
-            } else {
-                high = mid;
+        long totalCost = 0;
+        for(int c : cost) totalCost += c;
+
+        long acc = 0;
+        int target = 0;
+        for(int i = 0; i < n; i++){
+            acc += arr[i][1];
+            if(acc * 2 >= totalCost){
+                target = arr[i][0];
+                break;
             }
         }
 
-        return totalCost(nums, cost, low);
-    }
-
-    private long totalCost(int[] nums, int[] cost, int x){
-        long res = 0;
-        for(int i = 0; i < nums.length; i++){
-            res += (long)Math.abs(nums[i] - x) * cost[i];
+        long result = 0;
+        for(int i = 0; i < n; i++){
+            result += (long)Math.abs(nums[i] - target) * cost[i];
         }
-        return res;
+
+        return result;
     }
 }
