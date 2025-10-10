0a657d5faad71fdb08c3a244d7170b053f60cda1
Shreeja Singh <145353903+ShreejaSingh1@users.noreply.github.com>
2025-10-10 14:59:33 +0530

    Time: 17 ms (5.65%) | Memory: 43.2 MB (8.72%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.java
+++ 299-bulls-and-cows/bulls-and-cows.java
@@ -1,30 +1,28 @@
+import java.util.*;
+
 class Solution {
     public String getHint(String secret, String guess) {
-       int bulls = 0;
+        int bulls = 0;
         int cows = 0;
-        int[] secretCount = new int[10]; 
-        int[] guessCount = new int[10];
 
+        List<Character> secretList = new ArrayList<>();
+        List<Character> guessList = new ArrayList<>();
         for (int i = 0; i < secret.length(); i++) {
             if (secret.charAt(i) == guess.charAt(i)) {
-                bulls++; 
+                bulls++;
             } else {
-                secretCount[secret.charAt(i) - '0']++;
-                guessCount[guess.charAt(i) - '0']++;
+                secretList.add(secret.charAt(i));
+                guessList.add(guess.charAt(i));
             }
         }
 
-        for (int i = 0; i < 10; i++) {
-            cows += Math.min(secretCount[i], guessCount[i]); 
+        for (char ch : guessList) {
+            if (secretList.contains(ch)) {
+                cows++;
+                secretList.remove((Character) ch); 
+            }
         }
 
         return bulls + "A" + cows + "B";
     }
 }
-
-
-
-
-
-
-    
